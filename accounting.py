d9ac8cafdbbcb79019b1efd93bc71b5b599b775a
AgneDJ <fizikee@gmail.com>
2023-12-01 00:26:08 -0800

    Simplyfied function and comments  added

--- accounting.py
+++ accounting.py
@@ -1,61 +1,21 @@
 melon_cost = 1.00
 
-customer1_name = "Joe"
-customer1_melons = 5
-customer1_paid = 5.00
 
-customer2_name = "Frank"
-customer2_melons = 6
-customer2_paid = 6.00
+def counting_payments(filename):
+    '''Takes file, reads it, takes values and counts if customer underpaid'''
+    file = open(filename)  # opens txt file
+    for line in file:  # loops through each file line
+        line = line.rstrip().split("|")  # removes whitespace on the right
+        customer_name = line[1]  # second item on the line defined as variable
+        customer_melons = line[2]  # third item on the line defined as variable
+        customer_paid = line[3]  # fourth item on the line defined as variable
+        customer_expected = int(customer_melons) * \
+            float(melon_cost)  # counting expected payment
+        if customer_expected != customer_paid:  # checks if expected payment is not equal to paid
+            print(
+                f"{customer_name} paid ${customer_paid}, expected ${customer_expected}")  # prints out f string with results
+    file.close()  # closes file
 
-customer3_name = "Sally"
-customer3_melons = 3
-customer3_paid = 3.00
 
-customer4_name = "Sean"
-customer4_melons = 9
-customer4_paid = 9.50
-
-customer5_name = "David"
-customer5_melons = 4
-customer5_paid = 4.00
-
-customer6_name = "Ashley"
-customer6_melons = 3
-customer6_paid = 2.00
-
-customer1_expected = customer1_melons * melon_cost
-if customer1_expected != customer1_paid:
-    print(f"{customer1_name} paid ${customer1_paid:.2f},",
-          f"expected ${customer1_expected:.2f}"
-          )
-
-customer2_expected = customer2_melons * melon_cost
-if customer2_expected != customer2_paid:
-    print(f"{customer2_name} paid ${customer2_paid:.2f},",
-          f"expected ${customer2_expected:.2f}"
-          )
-
-customer3_expected = customer3_melons * melon_cost
-if customer3_expected != customer3_paid:
-    print(f"{customer3_name} paid ${customer3_paid:.2f},",
-          f"expected ${customer3_expected:.2f}"
-          )
-
-customer4_expected = customer4_melons * melon_cost
-if customer4_expected != customer4_paid:
-    print(f"{customer4_name} paid ${customer4_paid:.2f},",
-          f"expected ${customer4_expected:.2f}"
-          )
-
-customer5_expected = customer5_melons * melon_cost
-if customer5_expected != customer5_paid:
-    print(f"{customer5_name} paid ${customer5_paid:.2f},",
-          f"expected ${customer5_expected:.2f}"
-          )
-
-customer6_expected = customer6_melons * melon_cost
-if customer6_expected != customer6_paid:
-    print(f"{customer6_name} paid ${customer6_paid:.2f},",
-          f"expected ${customer6_expected:.2f}"
-          )
+# calls function with text file name as an argument
+counting_payments("customer-orders.txt")
